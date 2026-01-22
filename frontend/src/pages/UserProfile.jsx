64fd83cb93434b38732d65144adce9d262512657
Harsh Jaiswal <harshjaiswal.prgm@gmail.com>
2026-01-22 12:50:57 +0530

    update111

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -4,7 +4,7 @@ import api from "../api/axios";
 import { ArrowLeft } from "lucide-react";
 import RevenueChart from "../components/RevenueChart";
 import AttendanceChart from "../components/AttendanceChart";
-import defaultAvatar from "../assets/DefaultAvatar.png";
+
 
 
 const UserProfile = () => {
